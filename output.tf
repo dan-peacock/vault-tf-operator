e903e7991d01315c7b2f039f6a25d4a536c0b64f
danpeacock <danpeacock96@gmail.com>
2022-08-24 15:18:16 +0100

    fix outputs 2

--- output.tf
+++ output.tf
@@ -2,5 +2,5 @@ output "backend" {
  value = data.terraform_remote_state.creds.vault_aws_secret_backend.aws.path
 }
 output "role" {
- value = data.terraform_remote_state.creds.vault_aws_secret_backend_role.admin.name
+ value = data.terraform_remote_state.creds.outputs.role
 }
