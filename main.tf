2302fde3fa43fcc06cde275bc0961259e6824aa8
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2022-08-30 15:25:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,24 +20,8 @@ provider "aws" {
 }
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 # Create AWS EC2 Instance
 resource "aws_instance" "main" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0e34bbddc66def5ac"
   instance_type = "t2.nano"
 }
